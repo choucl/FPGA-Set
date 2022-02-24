6396611b58df5e905b519a6cab1206af3aae8f82
Pong En-Yu <andrew0911114933@gmail.com>
2022-02-24 16:14:34 +0800

    Add Acc.v

---
+++ Acc.v
@@ -0,0 +1,25 @@
+module Acc(clk, rst, acc_en, hit, candidate);
+
+    input               clk, rst, acc_en;
+    input [3:0]         hit;
+    output reg [7:0]    candidate;
+    reg [7:0]           psum;
+
+    // candidate
+    always @(posedge clk) begin
+        if (rst)
+            candidate <= 8'b0;
+        else
+            candidate <= candidate + psum;
+    end
+
+    // psum
+    always @(*) begin
+        if ((clk && rst) || !acc_en)
+            psum = 8'b0;
+        else begin
+            psum = hit[0] + hit[1] + hit[2] + hit[3];
+        end
+    end
+
+endmodule
