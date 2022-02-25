3d5f8f3f3e3bcf5954af54e9069705c532f6bae0
choucl <clc25600@gmail.com>
2022-02-25 23:49:05 +0800

    Add indention inside the module statements

--- lu.v
+++ lu.v
@@ -10,25 +10,25 @@ module LU (
     output reg hit
 );
 
-reg aorb;
-reg aandb;
-reg bandc;
-reg aandc;
+    reg aorb;
+    reg aandb;
+    reg bandc;
+    reg aandc;
 
-always @(*) begin
+    always @(*) begin
 
-    aorb  = covered_i[2] | covered_i[1];
-    aandb = covered_i[2] & covered_i[1];
-    bandc = covered_i[1] & covered_i[0];
-    aandc = covered_i[2] & covered_i[0];
-    
-    case (mode_i)
-        `MODE1: hit = covered_i[2];
-        `MODE2: hit = aandb;
-        `MODE3: hit = aorb - aandb;
-        `MODE4: hit = aandb + bandc + aandc - &covered_i;
-        default: hit = 0;
-    endcase
-end
+        aorb  = covered_i[2] | covered_i[1];
+        aandb = covered_i[2] & covered_i[1];
+        bandc = covered_i[1] & covered_i[0];
+        aandc = covered_i[2] & covered_i[0];
+        
+        case (mode_i)
+            `MODE1: hit = covered_i[2];
+            `MODE2: hit = aandb;
+            `MODE3: hit = aorb - aandb;
+            `MODE4: hit = aandb + bandc + aandc - &covered_i;
+            default: hit = 0;
+        endcase
+    end
 
 endmodule
--- pe.v
+++ pe.v
@@ -10,37 +10,37 @@ module PE (
     output [`COVERED_SZ-1:0] covered_o         // output of coverage
 );
 
-reg [`SQR_SZ:0] a_sqr;
-reg [`SQR_SZ:0] b_sqr;
-reg [`SQR_SZ:0] c_sqr;
-reg [`SQR_SZ:0] ra_sqr;
-reg [`SQR_SZ:0] rb_sqr;
-reg [`SQR_SZ:0] rc_sqr;
+    reg [`SQR_SZ:0] a_sqr;
+    reg [`SQR_SZ:0] b_sqr;
+    reg [`SQR_SZ:0] c_sqr;
+    reg [`SQR_SZ:0] ra_sqr;
+    reg [`SQR_SZ:0] rb_sqr;
+    reg [`SQR_SZ:0] rc_sqr;
 
-always @(*) begin
+    always @(*) begin
 
-    a_sqr  = (coord_i[`X_COORD] - cent_buf_i[`A_X]) 
-           * (coord_i[`X_COORD] - cent_buf_i[`A_X]);
-    a_sqr += (coord_i[`Y_COORD] - cent_buf_i[`A_Y]) 
-           * (coord_i[`Y_COORD] - cent_buf_i[`A_Y]);
-    ra_sqr = r_buf_i[`A_R] * r_buf_i[`A_R];
-    
-    b_sqr  = (coord_i[`X_COORD] - cent_buf_i[`B_X]) 
-           * (coord_i[`X_COORD] - cent_buf_i[`B_X]);
-    b_sqr += (coord_i[`Y_COORD] - cent_buf_i[`B_Y]) 
-           * (coord_i[`Y_COORD] - cent_buf_i[`B_Y]);
-    rb_sqr = r_buf_i[`B_R] * r_buf_i[`B_R];
-    
-    c_sqr  = (coord_i[`X_COORD] - cent_buf_i[`C_X]) 
-           * (coord_i[`X_COORD] - cent_buf_i[`C_X]);
-    c_sqr += (coord_i[`Y_COORD] - cent_buf_i[`C_Y]) 
-           * (coord_i[`Y_COORD] - cent_buf_i[`C_Y]);
-    rc_sqr = r_buf_i[`C_R] * r_buf_i[`C_R];
+        a_sqr  = (coord_i[`X_COORD] - cent_buf_i[`A_X]) 
+               * (coord_i[`X_COORD] - cent_buf_i[`A_X]);
+        a_sqr += (coord_i[`Y_COORD] - cent_buf_i[`A_Y]) 
+               * (coord_i[`Y_COORD] - cent_buf_i[`A_Y]);
+        ra_sqr = r_buf_i[`A_R] * r_buf_i[`A_R];
+        
+        b_sqr  = (coord_i[`X_COORD] - cent_buf_i[`B_X]) 
+               * (coord_i[`X_COORD] - cent_buf_i[`B_X]);
+        b_sqr += (coord_i[`Y_COORD] - cent_buf_i[`B_Y]) 
+               * (coord_i[`Y_COORD] - cent_buf_i[`B_Y]);
+        rb_sqr = r_buf_i[`B_R] * r_buf_i[`B_R];
+        
+        c_sqr  = (coord_i[`X_COORD] - cent_buf_i[`C_X]) 
+               * (coord_i[`X_COORD] - cent_buf_i[`C_X]);
+        c_sqr += (coord_i[`Y_COORD] - cent_buf_i[`C_Y]) 
+               * (coord_i[`Y_COORD] - cent_buf_i[`C_Y]);
+        rc_sqr = r_buf_i[`C_R] * r_buf_i[`C_R];
 
-end
+    end
 
-assign covered_o[2] = (ra_sqr >= a_sqr);
-assign covered_o[1] = (rb_sqr >= b_sqr);
-assign covered_o[0] = (rc_sqr >= c_sqr);
+    assign covered_o[2] = (ra_sqr >= a_sqr);
+    assign covered_o[1] = (rb_sqr >= b_sqr);
+    assign covered_o[0] = (rc_sqr >= c_sqr);
 
 endmodule
